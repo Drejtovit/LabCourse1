fb914e4a3ac343f3a60271bf563b7bcdb5dc4095
Drejtovit <drejtarron@gmail.com>
2025-09-09 17:35:14 +0200

    Completed authentication(sessions and tokens)

--- middleware.js
+++ middleware.js
@@ -1,1 +1,23 @@
-export { auth as middleware } from "@/lib/auth.js"
+
+import { auth } from "@/lib/auth.js";
+import { NextResponse } from "next/server";
+
+export default async function middleware(req) {
+    const userauth = await auth();
+
+    // if (!userauth) {
+    //   return NextResponse.redirect(new URL("/signin", req.url));
+    // }
+    if (userauth && req.nextUrl.pathname === "/register" || userauth && req.nextUrl.pathname === "/signin") {
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/register", "/signin"],
+};
+
+
+
+
